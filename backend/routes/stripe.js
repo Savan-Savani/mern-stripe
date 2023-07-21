722924f49c3f83f99074798b9196c082aa49173c
Savan <savansavani111@gmail.com>
2023-07-21 14:42:13 -0400

    development build

---
+++ backend/routes/stripe.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const Stripe = require("stripe");
+
+require("dotenv").config();
+const stripe = Stripe(process.env.STRIPE_KEY);
+
+const router = express.Router();
+
+router.post("/create-checkout-session", async (req, res) => {
+  const line_items = req.body.cartItems.map((item) => {
+    return {
+      price_data: {
+        currency: "cad",
+        product_data: {
+          name: item.name,
+          images: [item.image],
+          description: item.desc,
+          metadata: {
+            id: item.id,
+          },
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.cartQuantity,
+    };
+  });
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    phone_number_collection: {
+      enabled: true,
+    },
+    mode: "payment",
+    success_url: `${process.env.CLIENT_URL}/checkout-success`,
+    cancel_url: `${process.env.CLIENT_URL}/cart`,
+  });
+
+  res.send({ url: session.url });
+});
+
+module.exports = router;
